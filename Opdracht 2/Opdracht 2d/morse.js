d5da778e4865a73c39b6f465dc919fbb1b4dbd4a
Stefan <stefan.meijer3@student.nhlstenden.com>
2022-09-08 23:58:28 +0200

    Opdracht 2D

---
+++ Opdracht 2/Opdracht 2d/morse.js
@@ -0,0 +1,49 @@
+/**
+ * Class to morse text
+ * to test, write in console: const morse = new Morse('Hello World');
+ */
+class Morse {
+    constructor(text) {
+        this.text = text;
+        this.morsedText = '';
+        //List of morse codes
+        this.morsecodes = {
+            'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..',
+            'e': '.', 'f': '..-.', 'g': '--.', 'h': '....',
+            'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..',
+            'm': '--', 'n': '-.', 'o': '---', 'p': '.--.',
+            'q': '--.-', 'r': '.-.', 's': '...', 't': '-',
+            'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-',
+            'y': '-.--', 'z': '--..', ' ': '/',
+            '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+            '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+            '9': '----.', '0': '-----'
+        }
+    }
+
+    /**
+     * function to convert text to morse
+     * @returns {string} morse code
+     */
+    textToMorse() {
+        let textToMorse = this.getText().toLowerCase().split('');
+        
+        for (let i = 0; i < textToMorse.length; i++) {
+            this.morsedText += this.morsecodes[textToMorse[i]] + ' ';
+        }
+
+        return this.morsedText;
+    }
+
+    //Getters and setters
+    setText(value) {
+        this.text = value;
+    }
+    getText() {
+        return this.text;
+    }
+
+    getMorsedText() {
+        return this.morsedText;
+    }
+}
